e6bf67b7dfcdf03c75b52c6f140383ac1e364577
Mohamed Slimani <slimani.m1993@gmail.com>
2022-08-06 19:04:55 +0100

    add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
     plugins: [
+        VitePWA(),
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
