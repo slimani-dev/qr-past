110e414a1c227b3af78ac7e59c71278e30d340b3
Mohamed Slimani <slimani.m1993@gmail.com>
2022-08-06 20:38:41 +0100

    trying to make pwa work

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,30 @@ export default defineConfig({
     plugins: [
         VitePWA({
             registerType: 'autoUpdate',
-            injectRegister: 'auto'
+            injectRegister: 'auto',
+            manifest: {
+                name: 'QR Past',
+                short_name: 'QR Past',
+                description: 'Scan QR and Past content',
+                theme_color: '#32138D',
+                scope: '/',
+                start_url: '/',
+                icons: [
+                    {
+                        src: '/icons/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            },
+            devOptions: {
+                enabled: true
+            }
         }),
         laravel({
             input: 'resources/js/app.js',
