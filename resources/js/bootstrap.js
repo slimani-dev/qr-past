6e24779cc58e32a597fab9ce61a228d4b240e4d0
Mohamed Slimani <slimani.m1993@gmail.com>
2022-08-06 19:11:51 +0100

    fix h-screen

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,3 +36,10 @@ window.Echo = new Echo({
     enabledTransports: ['ws', 'wss'],
     disableStats: true,
 })
+
+let vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+})
