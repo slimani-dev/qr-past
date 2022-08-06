fa5052345f34712d9aca573b7716245a72b8337b
Mohamed Slimani <slimani.m1993@gmail.com>
2022-08-06 21:07:55 +0100

    trying to make pwa work

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -43,3 +43,6 @@ window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
 })
+
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
